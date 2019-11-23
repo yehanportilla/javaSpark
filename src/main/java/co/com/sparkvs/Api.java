0326fd2b7d07df7f867c50d973aaa6e8fa949c0f
sergio <ext_svillami@mercacolibre.com>
2019-11-23 18:24:18 -0500

    manejo de rutas separadas en un solo archivo

--- src/main/java/co/com/sparkvs/Api.java
+++ src/main/java/co/com/sparkvs/Api.java
@@ -1,5 +1,32 @@
 package co.com.sparkvs;
-/*
+
+import com.google.gson.Gson;
+
+import co.com.sparkvs.controller.BlogController;
+import static spark.Spark.*;
+
+
 public class Api {
+	
+	
+	
+	 public static void main(String[] args) {
+		 
+		
+		 Gson gson = new Gson();
+			
+			path("/api", () -> {
+				
+				path("/blog", () -> {
+					
+					 get("",(req,res)->{return BlogController.listar(req, res);},gson ::toJson );
+					 get("/:username",(req,res)->{return BlogController.listarUsername(req, res);},gson::toJson);		 
+					 post("", (req, res)->{ return BlogController.guardar(req,res); }, gson ::toJson);
+					 delete("/:username",(req,res)->{return BlogController.eliminar(req, res);},gson::toJson);
+			        
+			    });
+			});
+		 
+		 
+	 }
 }
-*/
--- src/main/java/co/com/sparkvs/controller/BlogController.java
+++ src/main/java/co/com/sparkvs/controller/BlogController.java
@@ -2,53 +2,59 @@ package co.com.sparkvs.controller;
 
 import co.com.sparkvs.model.Blog;
 import co.com.sparkvs.service.BlogService;
+import spark.Request;
+import spark.Response;
+
 import com.google.gson.Gson;
 
 import static spark.Spark.*;
 
 public class BlogController {
-    public  static BlogService blogService = new BlogService();
-
-    public static void main(String[] args) {
-        Gson gson = new Gson();
+	
+	public static BlogService blogService = new BlogService();
+ 	public static Gson gson = new Gson();
+ 	
+ 	
         /**
          * Metodo encargado de guardar blog
          */
-        post("/guardar", (req, res)->{
-            res.type("aplication/json");
-            Blog blog = gson.fromJson(req.body(), Blog.class);
-            return blogService.saveBlog(blog);
-        }, gson ::toJson);
+        public static Object guardar(Request req, Response res) {
+    		res.type("aplication/json");
+    		Blog blog = gson.fromJson(req.body(), Blog.class);
+    		return blogService.saveBlog(blog);
+
+    	}
 
         /**
          * Metodo encargado de listar todos los blog
          */
-        get("/listar",(req, res) ->{
-            res.type("aplication/json");
-            return blogService.listBlog();
-        }, gson ::toJson);
+        public static Object listar(Request req, Response res) {
+    		res.type("aplication/json");
+    		return blogService.listBlog();
+
+    	}
 
         /**
          * Metodo encargado de obtener blog por nombre de usuario
          */
-        get("/listar/:username",(req, res)->{
-            res.type("aplication/json");
-            Blog blog = blogService.getBlogByUserName(req.params("username"));
-            if(blog != null){
-                return blog;
-            }
-             else{
-                 return "No se encontraron datos.";
-            }
-        }, gson ::toJson);
+        public static Object listarUsername(Request req, Response res) {
+    		res.type("aplication/json");
+    		Blog blog = blogService.getBlogByUserName(req.params("username"));
+    		if (blog != null) {
+    			return blog;
+    		} else {
+    			return "No se encontraron datos.";
+    		}
+    	}
 
         /**
          * Metodo encargado de eliminar blog por user name
          */
-        delete("/eliminar/:username",(req, res)->{
-            res.type("aplication/json");
-             blogService.deleteBlogById(req.params("username"));
-            return "Blog eliminado con exito...!";
-        });
-    }
+    	public static Object eliminar(Request req, Response res) {
+    		res.type("aplication/json");
+    		blogService.deleteBlogById(req.params("username"));
+    		return "Blog eliminado con exito...!";
+    	}
+    	
+    
 }
